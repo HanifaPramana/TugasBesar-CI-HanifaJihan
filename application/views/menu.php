46b49ce6790ed541a923e6ac0be24be052501a28
HanifaPramana <hanifapramana17@gmail.com>
2018-07-02 10:59:06 +0700

    Menambahkan Datatables + edit halaman admin

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo" href="index.html"><img src="<?php echo base_url('assets_admin/') ?>images/logo.svg" alt="logo"/></a>
+        <a class="navbar-brand brand-logo" href="index.html"><img src="<?php echo base_url('assets_admin/') ?>images/logokost.png"  alt="logo"/ ></a>
         <a class="navbar-brand brand-logo-mini" href="index.html"><img src="<?php echo base_url('assets_admin/') ?>images/logo-mini.svg" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-stretch">
@@ -162,21 +162,8 @@
       <!-- partial:partials/_sidebar.html -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
-          <li class="nav-item nav-profile">
-            <a href="#" class="nav-link">
-              <div class="nav-profile-image">
-                <img src="<?php echo base_url('assets_admin/') ?>images/faces/face1.jpg" alt="profile">
-                <span class="login-status online"></span> <!--change to offline or busy as needed-->              
-              </div>
-              <div class="nav-profile-text d-flex flex-column">
-                <span class="font-weight-bold mb-2">David Grey. H</span>
-                <span class="text-secondary text-small">Project Manager</span>
-              </div>
-              <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-            </a>
-          </li>
           <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="<?php echo site_url();?>/dashboard">
               <span class="menu-title">Dashboard</span>
               <i class="mdi mdi-home menu-icon"></i>
             </a>
